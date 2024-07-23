12dd748a0e71e30e10ff0a4a30b74a922dd4245e
Yongjun Lee <iamdragonjun@gmail.com>
2024-07-23 15:45:12 +0200

    Update script.js
    모바일 환경에서 손가락 드래그할때 클릭되는 것 오류 수정

--- script.js
+++ script.js
@@ -6,22 +6,43 @@ document.addEventListener('DOMContentLoaded', function () {
 
     portfolioItems.forEach(item => {
         let touched = false;
+        let touchStartX = 0;
+        let touchStartY = 0;
 
         item.addEventListener('touchstart', function (e) {
-            if (!touched) {
-                e.preventDefault();
-                e.stopPropagation();
-                portfolioItems.forEach(otherItem => {
-                    if (otherItem !== item) {
-                        otherItem.querySelector('.overlay').style.opacity = '0';
-                        otherItem.dataset.touched = false;
-                    }
-                });
-                item.querySelector('.overlay').style.opacity = '1';
-                item.dataset.touched = true;
-                touched = true;
-            } else {
-                window.location.href = item.closest('a').href;
+            touched = true;
+            touchStartX = e.touches[0].clientX;
+            touchStartY = e.touches[0].clientY;
+        }, false);
+
+        item.addEventListener('touchmove', function () {
+            touched = false; // 터치 이동 중에는 터치 상태를 해제
+        }, false);
+
+        item.addEventListener('touchend', function (e) {
+            if (!touched) return;
+
+            const touchEndX = e.changedTouches[0].clientX;
+            const touchEndY = e.changedTouches[0].clientY;
+            const diffX = Math.abs(touchEndX - touchStartX);
+            const diffY = Math.abs(touchEndY - touchStartY);
+
+            // 이동 거리가 10px 이하일 경우에만 클릭으로 간주
+            if (diffX < 10 && diffY < 10) {
+                if (item.dataset.touched === 'true') {
+                    window.location.href = item.closest('a').href;
+                } else {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    portfolioItems.forEach(otherItem => {
+                        if (otherItem !== item) {
+                            otherItem.querySelector('.overlay').style.opacity = '0';
+                            otherItem.dataset.touched = 'false';
+                        }
+                    });
+                    item.querySelector('.overlay').style.opacity = '1';
+                    item.dataset.touched = 'true';
+                }
             }
         }, false);
 
